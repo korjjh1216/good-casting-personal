6d74b8bdf7192b772cd382672f2e7416d9bfcd9f
korjjh1216 <korjjh1216@naver.com>
2021-06-11 14:52:17 +0900

    aws 업로드전 backup

--- fe/src/pages/actor-register.js
+++ fe/src/pages/actor-register.js
@@ -9,21 +9,15 @@ import FileUploads from '../components/Core/FileUploads';
 import PageWrapper from '../components/PageWrapper';
 
 import { useDispatch, useSelector } from 'react-redux';
-import {
-    profileRegister,
-    profileSelector,
-    resetStatus,
-} from '../state/reducer/profile.reducer';
-import {
-    fileSelector,
-    resetFile,
-    setFirst,
-} from '../state/reducer/file.reducer';
+import { profileRegister, profileSelector, resetStatus } from '../state/reducer/profile.reducer';
+import { fileSelector, resetFile, setFirst } from '../state/reducer/file.reducer';
 
 import '../scss/css/fileUpload.css';
 
 const ProfileRegister = () => {
     const dispatch = useDispatch();
+
+    const userInfo = typeof window !== `undefined` ? JSON.parse(localStorage.getItem('USER')) : null;
 
     const profileState = useSelector(profileSelector);
     const fileList = useSelector(fileSelector).fileList;
@@ -98,37 +92,25 @@ const ProfileRegister = () => {
                     <div className="container">
                         <div className="mb-15 mb-lg-23">
                             <div className="dashboard-main-container mt-25 mt-lg-31">
-                                <h5 className="font-size-6 font-weight-semibold mb-11">
-                                    프로필 등록
-                                </h5>
+                                <h5 className="font-size-6 font-weight-semibold mb-11">프로필 등록</h5>
                                 <div className="contact-form bg-white shadow-8 rounded-4 pl-sm-10 pl-4 pr-sm-11 pr-4 pt-15 pb-13">
                                     <div className="upload-file mb-16 text-center">
                                         <div className="m-auto px-6 mb-7">
-                                            <FileUpload
-                                                setImages={setImages}
-                                                image={image}
-                                            />
+                                            <FileUpload setImages={setImages} image={image} />
                                         </div>
                                         <p>※ 프로필 사진을 등록해주세요</p>
                                     </div>
                                     <form onSubmit={handleSubmit}>
                                         <fieldset>
                                             <div className="col-md-12">
-                                                <label
-                                                    htmlFor="aboutTextarea"
-                                                    className="d-block text-black-2 font-size-4 font-weight-semibold mb-4"
-                                                >
-                                                    추가 사진 / 연기 동영상
-                                                    업로드
+                                                <label htmlFor="aboutTextarea" className="d-block text-black-2 font-size-4 font-weight-semibold mb-4">
+                                                    추가 사진 / 연기 동영상 업로드
                                                 </label>
                                                 <FileUploads image={image} />
                                             </div>
                                             <div className="col-md-12">
                                                 <div className="form-group">
-                                                    <label
-                                                        htmlFor="aboutTextarea"
-                                                        className="d-block text-black-2 font-size-4 font-weight-semibold mb-4"
-                                                    >
+                                                    <label htmlFor="aboutTextarea" className="d-block text-black-2 font-size-4 font-weight-semibold mb-4">
                                                         경력
                                                     </label>
                                                     <ProfileCareer />
@@ -136,54 +118,22 @@ const ProfileRegister = () => {
                                             </div>
                                             <div className="col-md-12">
                                                 <div className="form-group">
-                                                    <label
-                                                        htmlFor="aboutTextarea"
-                                                        className="d-block text-black-2 font-size-4 font-weight-semibold mb-4"
-                                                    >
+                                                    <label htmlFor="aboutTextarea" className="d-block text-black-2 font-size-4 font-weight-semibold mb-4">
                                                         자기소개
                                                     </label>
-                                                    <textarea
-                                                        name="contents"
-                                                        id="aboutTextarea"
-                                                        cols="30"
-                                                        rows="7"
-                                                        className="border border-mercury text-gray w-100 pt-4 pl-6"
-                                                        placeholder="간단한 자기소개를 입력해주세요"
-                                                        onChange={handleChange}
-                                                        value={inputs.contents}
-                                                    ></textarea>
+                                                    <textarea name="contents" id="aboutTextarea" cols="30" rows="7" className="border border-mercury text-gray w-100 pt-4 pl-6" placeholder="간단한 자기소개를 입력해주세요" onChange={handleChange} value={inputs.contents}></textarea>
                                                 </div>
                                             </div>
                                             <div className="col-md-12">
                                                 <div className="form-group">
-                                                    <label
-                                                        htmlFor="aboutTextarea"
-                                                        className="d-block text-black-2 font-size-4 font-weight-semibold mb-4"
-                                                    >
+                                                    <label htmlFor="aboutTextarea" className="d-block text-black-2 font-size-4 font-weight-semibold mb-4">
                                                         공개/비공개 설정
                                                     </label>
-                                                    <FormControlLabel
-                                                        control={
-                                                            <Switch
-                                                                color="primary"
-                                                                checked={
-                                                                    inputs.privacy
-                                                                }
-                                                                onChange={
-                                                                    handleToggle
-                                                                }
-                                                                name="privacy"
-                                                            />
-                                                        }
-                                                        label="공개"
-                                                    />
+                                                    <FormControlLabel control={<Switch color="primary" checked={inputs.privacy} onChange={handleToggle} name="privacy" />} label="공개" />
                                                 </div>
                                             </div>
                                             <div className="row">
-                                                <button
-                                                    type="submit"
-                                                    className="btn btn-green btn-h-60 text-white min-width-px-210 rounded-5 text-uppercase btn-center"
-                                                >
+                                                <button type="submit" className="btn btn-green btn-h-60 text-white min-width-px-210 rounded-5 text-uppercase btn-center">
                                                     등록하기
                                                 </button>
                                             </div>
